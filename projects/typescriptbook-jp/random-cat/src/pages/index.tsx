c50dbd50f740e78173f0632325ab02580a9ebad4
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-08-08 00:16:21 +0900

    Add a tutorial
    The tutorial for next with typescript

---
+++ projects/typescriptbook-jp/random-cat/src/pages/index.tsx
@@ -0,0 +1,60 @@
+import { GetServerSideProps, NextPage } from "next";
+import { useState } from "react";
+
+interface CatCategory {
+  id: number;
+  name: string;
+}
+
+interface SearchCatImage {
+  breeds: string[];
+  categories: CatCategory[];
+  id: string;
+  url: string;
+  width: number;
+  height: number;
+}
+
+interface IndexPageProps {
+  initialCatImageUrl: string;
+}
+
+type SearchCatImageResponse = SearchCatImage[];
+
+const fetchCatImage = async (): Promise<SearchCatImage> => {
+  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const result = (await res.json()) as SearchCatImageResponse;
+  return result[0];
+};
+
+const IndexPage: NextPage<IndexPageProps> = ({ initialCatImageUrl }) => {
+  const [catImageUrl, setCatImageUrl] = useState(initialCatImageUrl);
+
+  const handleClick = async () => {
+    const image = await fetchCatImage();
+    setCatImageUrl(image.url);
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>きょうのにゃんこ🐱</button>
+      <div style={{ marginTop: 8 }}>
+        <img src={catImageUrl} />
+      </div>
+    </div>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps<
+  IndexPageProps
+> = async () => {
+  const catImage = await fetchCatImage();
+
+  return {
+    props: {
+      initialCatImageUrl: catImage.url,
+    },
+  };
+};
+
+export default IndexPage;
