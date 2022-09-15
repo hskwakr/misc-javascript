5e29fd4d1d7b5788cd8c36c6b857ec6473c242dd
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-09-15 22:07:01 +0900

    Create sample-2.js

---
+++ projects/javascript-primer/Promise-Async/src/3_async/sample-2.js
@@ -0,0 +1,8 @@
+// 関数宣言のAsync Function版
+async function fn1() {}
+// 関数式のAsync Function版
+const fn2 = async function () {};
+// Arrow FunctionのAsync Function版
+const fn3 = async () => {};
+// メソッドの短縮記法のAsync Function版
+const obj = { async method() {} };
