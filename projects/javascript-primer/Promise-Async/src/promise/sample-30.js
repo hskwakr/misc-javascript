53d45a19b508b01ec35eb4bba8e7f5092b1a0ed0
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-09-15 20:21:10 +0900

    Create sample-30.js

---
+++ projects/javascript-primer/Promise-Async/src/promise/sample-30.js
@@ -0,0 +1,23 @@
+function dummyFetch(path) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (path.startsWith("/resource")) {
+        resolve({ body: `Response body of ${path}` });
+      } else {
+        reject(new Error("NOT FOUND"));
+      }
+    }, 1000 * Math.random());
+  });
+}
+
+const fetchedPromise = Promise.all([
+  dummyFetch("/resource/A"),
+  dummyFetch("/not_found/B"), // Bは存在しないため失敗する
+]);
+fetchedPromise
+  .then(([responseA, responseB]) => {
+    // この行は実行されません
+  })
+  .catch((error) => {
+    console.error(error); // Error: NOT FOUND
+  });
