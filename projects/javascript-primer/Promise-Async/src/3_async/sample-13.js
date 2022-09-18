eae468e533ce8fc6a345e6a7c20bb4529fba4ef1
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-09-18 19:42:56 +0900

    Create sample-13.js

---
+++ projects/javascript-primer/Promise-Async/src/3_async/sample-13.js
@@ -0,0 +1,14 @@
+async function asyncMain() {
+  // 中でawaitしても、Async Functionの外側の処理まで止まるわけではない
+  await new Promise((resolve) => {
+    setTimeout(resolve, 16);
+  });
+}
+
+console.log("1. asyncMain関数を呼び出します");
+// Async Functionは外から見れば単なるPromiseを返す関数
+asyncMain().then(() => {
+  console.log("3. asyncMain関数が完了しました");
+});
+// Async Functionの外側の処理はそのまま進む
+console.log("2. asyncMain関数外では、次の行が同期的に呼び出される");
