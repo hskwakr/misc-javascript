11800a2459014330d45a80c557c4f2922def6240
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-09-19 00:36:55 +0900

    Create sample-16.js

---
+++ projects/javascript-primer/Promise-Async/src/3_async/sample-16.js
@@ -0,0 +1,7 @@
+// <script type="module">
+
+// console.log("実行開始");
+// const startTime = Date.now();
+// // awaitを使って1秒待つ
+// await new Promise((resolve) => setTimeout(resolve, 1000));
+// console.log(`実行終了: ${Date.now() - startTime}ms 経過しました`);
