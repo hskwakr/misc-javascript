de8e30e1d24d9d1de76bc67f5b10aa9044220891
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-09-21 01:47:02 +0900

    Update sample-1.ts

--- projects/typescriptbook-jp/learn-with-reading/src/oop/sample-1.ts
+++ projects/typescriptbook-jp/learn-with-reading/src/oop/sample-1.ts
@@ -25,3 +25,40 @@
 // }
 
 // new Person("Alice");
+
+/* ================================= */
+/* field                             */
+/* ================================= */
+
+// javascript
+// class Person {}
+// const alice = new Person();
+// alice.name = "Alice";
+
+// class Person {
+//   name: string;
+// }
+// const alice = new Person();
+// alice.name = "Alice";
+
+// class Person {}
+// const person = new Person();
+// console.log(person.age); // error
+// // Property 'age' does not exist on type 'Person'.
+
+// class Person {
+//   private name;
+
+//   constructor(name: string) {
+//     this.name = name;
+//   }
+// }
+
+// // strictNullChecks
+// // strictPropertyInitialization
+// class Person {
+//   name: string; // error
+//   // Property 'name' has no initializer and is not definitely assigned in the constructor.
+// }
+// const alice = new Person();
+// console.log(alice.name);
