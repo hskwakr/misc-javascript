7227e795b38753e5f96cedda3101c4f8076e150c
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-09-22 03:36:51 +0900

    Create sample-2.ts

---
+++ projects/typescriptbook-jp/learn-with-reading/src/oop/sample-2.ts
@@ -0,0 +1,29 @@
+/* ================================= */
+/* class                             */
+/* initializer                       */
+/* ================================= */
+
+// class Point {
+//   x: number = 0;
+
+//   y: number = 0;
+// }
+
+// const point = new Point();
+// console.log(point.x, point.y);
+
+// class Point {
+//   x: number;
+
+//   y: number;
+
+//   constructor() {
+//     this.x = 0;
+//     this.y = 0;
+//   }
+// }
+
+// class Point {
+//   x = 0; // number型と型推論される
+// }
+
