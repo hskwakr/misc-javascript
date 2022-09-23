751dc4d26df4a54be53de7cb3540ad7c2448a21f
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-09-24 07:45:46 +0900

    Create sample-3.ts

---
+++ projects/typescriptbook-jp/learn-with-reading/src/oop/sample-3.ts
@@ -0,0 +1,49 @@
+/* eslint-disable max-len */
+/* ================================= */
+/* class                             */
+/* constructor shorthand             */
+/* ================================= */
+
+// class ConstructorInAccessModifier {
+//   constructor(
+//     arg0: number,
+//     public arg1: number,
+//     protected arg2: number,
+//     private arg3: number
+//   ) {
+//     console.log({ arg0, arg1, arg2, arg3 });
+//   }
+// }
+
+// class ConstructorOutAccessModifier {
+//   public arg1: number;
+
+//   protected arg2: number;
+
+//   private arg3: number;
+
+//   constructor(arg0: number, arg1: number, arg2: number, arg3: number) {
+//     this.arg1 = arg1;
+//     this.arg2 = arg2;
+//     this.arg3 = arg3;
+//     console.log({ arg0, arg1, arg2, arg3 });
+//   }
+// }
+
+// const InAccess = new ConstructorInAccessModifier(1, 2, 3, 4);
+// InAccess.arg0;
+// // Property 'arg0' does not exist on type 'ConstructorInAccessModifier'.
+// InAccess.arg1;
+// InAccess.arg2;
+// // Property 'arg2' is protected and only accessible within class 'ConstructorInAccessModifier' and its subclasses.
+// InAccess.arg3;
+// // Property 'arg3' is private and only accessible within class 'ConstructorInAccessModifier'.
+
+// const outAccess = new ConstructorOutAccessModifier(1, 2, 3, 4);
+// outAccess.arg0;
+// // Property 'arg0' does not exist on type 'ConstructorOutAccessModifier'.
+// outAccess.arg1;
+// outAccess.arg2;
+// // Property 'arg2' is protected and only accessible within class 'ConstructorOutAccessModifier' and its subclasses.
+// outAccess.arg3;
+// // Property 'arg3' is private and only accessible within class 'ConstructorOutAccessModifier'.
