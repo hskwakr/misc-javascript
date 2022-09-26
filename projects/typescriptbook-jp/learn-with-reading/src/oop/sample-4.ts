a17e7e7ba46a69ae439dd1ab865102ab8576e287
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-09-26 22:15:01 +0900

    Create sample-4.ts

---
+++ projects/typescriptbook-jp/learn-with-reading/src/oop/sample-4.ts
@@ -0,0 +1,27 @@
+/* eslint-disable max-classes-per-file */
+/* ================================= */
+/* inheritance                       */
+/* ================================= */
+
+// class Parent {}
+// class Child extends Parent {}
+
+// class Parent {}
+// class Child extends Parent {
+//   constructor() {
+//     super();
+//   }
+// }
+
+/* ================================= */
+/* instanceof                        */
+/* ================================= */
+
+// const parent1 = new Parent();
+// const child = new Child();
+
+// console.log(parent1 instanceof Parent); // true
+// console.log(parent1 instanceof Child); // false
+
+// console.log(child instanceof Parent); // true
+// console.log(child instanceof Child); // true
