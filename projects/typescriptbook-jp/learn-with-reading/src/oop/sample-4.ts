a42c64bc4cefba7e5e51b8a1eb17a9b745f5226f
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-09-27 01:39:19 +0900

    Update sample-4.ts

--- projects/typescriptbook-jp/learn-with-reading/src/oop/sample-4.ts
+++ projects/typescriptbook-jp/learn-with-reading/src/oop/sample-4.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable max-classes-per-file */
 /* ================================= */
 /* inheritance                       */
@@ -49,3 +50,25 @@
 // const userId: UserId = new GroupId("...");
 // // Type 'GroupId' is not assignable to type 'UserId'.
 // // Types have separate declarations of a private property 'id'.
+
+/* ================================= */
+/* abstract class                    */
+/* ================================= */
+
+// abstract class Food {
+//   constructor(protected name: string, protected calorie: number) {}
+
+//   showDebug() {
+//     console.log(`name = ${this.name} `);
+//     console.log(`calorie = ${this.calorie}kcal `);
+//   }
+//   abstract keepRefrigerated(): boolean;
+// }
+// class Meat extends Food {}
+// // Non-abstract class 'Meat' does not implement inherited abstract member 'keepRefrigerated' from class 'Food'.
+
+// class Meat extends Food {
+//     keepRefrigerated(): boolean {
+//       return true;
+//     }
+//   }
