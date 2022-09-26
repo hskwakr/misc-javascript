bfff093cc5bda7409a05157501ca0e3e22c180aa
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-09-27 00:39:37 +0900

    Update sample-4.ts

--- projects/typescriptbook-jp/learn-with-reading/src/oop/sample-4.ts
+++ projects/typescriptbook-jp/learn-with-reading/src/oop/sample-4.ts
@@ -25,3 +25,27 @@
 
 // console.log(child instanceof Parent); // true
 // console.log(child instanceof Child); // true
+
+/* ================================= */
+/* nominal typing                    */
+/* ================================= */
+
+// class UserId {
+//   private readonly id: string;
+
+//   constructor(id: string) {
+//     this.id = id;
+//   }
+// }
+
+// class GroupId {
+//   private readonly id: string;
+
+//   constructor(id: string) {
+//     this.id = id;
+//   }
+// }
+
+// const userId: UserId = new GroupId("...");
+// // Type 'GroupId' is not assignable to type 'UserId'.
+// // Types have separate declarations of a private property 'id'.
