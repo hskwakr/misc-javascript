a61c8edc52652a43b3bf9868e85b08d614da651e
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-09-29 21:49:51 +0900

    Create sample-3.ts

---
+++ projects/typescriptbook-jp/learn-with-reading/src/builtin-api/sample-3.ts
@@ -0,0 +1,26 @@
+/* ================================= */
+/* Error                             */
+/* ================================= */
+
+// throw new Error();
+
+// throw "id is not string!";
+
+// class CustomeError extends Error {
+//   public constructor(message?: string) {
+//     super(message);
+//   }
+// }
+
+// const err: CustomeError = new CustomeError("FAILED!");
+// console.log(err.name);
+// console.log(err.message);
+// console.log(err.stack);
+
+// try {
+//   // ...
+// } catch (e) {
+//   if (e instanceof Error) {
+//     // ...
+//   }
+// }
