592d656500cd27974ed669cb22716cfde275e955
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-09-29 11:08:17 +0900

    Create sample-2.ts
    https://typescriptbook.jp/reference/builtin-api/set

---
+++ projects/typescriptbook-jp/learn-with-reading/src/builtin-api/sample-2.ts
@@ -0,0 +1,64 @@
+/* ================================= */
+/* Set<T>                            */
+/* ================================= */
+
+// const fruits = new Set(["apple", "orange", "banana"]);
+// console.log(fruits);
+
+// const fruits = new Set(["apple", "apple", "apple"]);
+// console.log(fruits);
+
+// const fruits = new Set();
+// console.log(fruits);
+
+// const fruits = new Set<string>();
+// //                    ^^^^^^^^ 型変数を指定
+
+// function doSomething(strings: Set<string>) {
+//   // ...
+// }
+
+// const fruits = new Set<string>();
+// fruits.add("apple");
+// fruits.add("apple");
+// console.log(fruits);
+
+// const numbers = new Set<number>();
+// numbers.add(1).add(2).add(3);
+// numbers.add(1);
+// console.log(numbers);
+
+// const numbers = new Set([1, 2, 3]);
+// numbers.delete(3);
+// console.log(numbers);
+
+// const numbers = new Set([1, 2, 3]);
+// console.log(numbers.has(1));
+// console.log(numbers.has(999));
+
+// const fruits = new Set(["apple", "orange", "banana"]);
+// console.log(fruits.size);
+
+// const fruits = new Set(["apple", "orange", "banana"]);
+// fruits.clear();
+// console.log(fruits);
+
+// const fruits = new Set(["apple", "orange", "banana"]);
+// for (const fruit of fruits) {
+//   console.log(fruit); // "apple"、"orange"、"banana"の順で出力される
+// }
+
+// const fruits = new Set(["apple", "orange", "banana"]);
+// const array = [...fruits];
+// console.log(array);
+
+// const fruits = new Set(["apple", "orange", "banana"]);
+// console.log(JSON.stringify(fruits)); // "{}"
+
+// const fruits = new Set(["apple", "orange", "banana"]);
+// const array = [...fruits];
+// console.log(JSON.stringify(array));
+
+// const array1 = [0, 0, 1, 1, 2, 2];
+// const array2 = [...new Set(array1)];
+// console.log(array2);
