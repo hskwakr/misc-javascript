bba4209d53db4b79f6abccbe9c8c19530f8ec304
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-10-01 01:29:18 +0900

    Update sample-1.ts
    https://typescriptbook.jp/reference/type-reuse/keyof-type-operator

--- projects/typescriptbook-jp/learn-with-reading/src/reuse-type/sample-1.ts
+++ projects/typescriptbook-jp/learn-with-reading/src/reuse-type/sample-1.ts
@@ -20,3 +20,36 @@
 // //     x: number;
 // //     y: number;
 // // }
+
+/* ================================= */
+/* keyof                            */
+/* ================================= */
+
+// type Person = {
+//   name: string;
+// };
+// type PersonKey = keyof Person;
+// // type PersonKey = "name"
+
+// type Book = {
+//   title: string;
+//   price: number;
+//   rating: number;
+// };
+// type BookKey = keyof Book;
+// // 上は次と同じ意味になる
+// type BookKey = "title" | "price" | "rating";
+
+// type MapLike = { [K: string]: any };
+// type MapKeys = keyof MapLike;
+// // type MapKeys = string | number
+
+// type MapLike = { [K in "x" | "y" | "z"]: any };
+// type MapKeys = keyof MapLike;
+// // type MapKeys = "x" | "y" | "z"
+
+// type What = keyof {};
+// // type What = never
+
+// type AnyKeys = keyof any;
+// // type AnyKeys = string | number | symbol
