9900f6f46a7897709fdc47dc04bbc3a6746564ff
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-10-04 03:02:58 +0900

    Create sample-1.ts
    https://typescriptbook.jp/reference/declaration-file

---
+++ projects/typescriptbook-jp/learn-with-reading/src/declaration-file/sample-1.ts
@@ -0,0 +1,18 @@
+/* ================================= */
+/* declaration file                  */
+/* ================================= */
+
+// interface Person {
+//   firstName: string;
+//   lastName: string;
+// }
+
+// function greeter(person: Person): string {
+//   return `Hello, ${person.firstName} ${person.lastName}`;
+// }
+
+// // interface Person {
+// //   firstName: string;
+// //   lastName: string;
+// // }
+// // declare function greeter(person: Person): string;
