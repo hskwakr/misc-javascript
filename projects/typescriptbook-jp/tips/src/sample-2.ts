85e425a05e800d322d37b84a6d29b3144ecbd28f
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-10-05 04:02:28 +0900

    Create sample-2.ts
    https://typescriptbook.jp/tips/merge-objects

---
+++ projects/typescriptbook-jp/tips/src/sample-2.ts
@@ -0,0 +1,25 @@
+/* ================================= */
+/* merge objects                     */
+/* ================================= */
+
+// const merged = { ...obj1, ...obj2 };
+
+// const merged = {
+//   ...obj1,
+//   ...obj2,
+//   ...obj3,
+//   // ...
+// };
+
+// const obj1: object = {
+//   firstName: "Otto",
+//   middleName: "von",
+//   lastName: "Bismarck",
+// };
+// const obj2: object = {
+//   firstName: "Yuko",
+//   lastName: "Sato",
+// };
+
+// const merged: object = { ...obj1, ...obj2 };
+// console.log(merged);
