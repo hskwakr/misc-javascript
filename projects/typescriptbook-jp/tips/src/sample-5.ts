0da1392ebcd28fa993e6a3d77dc1664ffe3249dc
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-10-05 10:49:06 +0900

    Create sample-5.ts
    https://typescriptbook.jp/tips/generates-type-from-object

---
+++ projects/typescriptbook-jp/tips/src/sample-5.ts
@@ -0,0 +1,21 @@
+/* ================================= */
+/* companion object                  */
+/* ================================= */
+
+// export type Rectangle = {
+//   height: number;
+//   width: number;
+// };
+// export const Rectangle = {
+//   from(height: number, width: number): Rectangle {
+//     return {
+//       height,
+//       width,
+//     };
+//   },
+// };
+
+// import { Rectangle } from "./rectangle";
+// const rec: Rectangle = Rectangle.from(1, 3);
+// console.log(rec.height); // 1
+// console.log(rec.width); // 3
