a48bd6d8ee8df0f23944acdf423f5e9e9330fbd5
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-10-05 10:55:09 +0900

    Create sample-7.ts
    https://typescriptbook.jp/tips/generates-type-from-object-key

---
+++ projects/typescriptbook-jp/tips/src/sample-7.ts
@@ -0,0 +1,29 @@
+/* ================================= */
+/* generates type from object key    */
+/* ================================= */
+
+// const conf = {
+//   en: "Are you sure?",
+//   fr: "Êtes-vous sûr?",
+//   es: "Está seguro?",
+//   ja: "よろしいですか？",
+//   zh: "您确定吗？",
+// };
+
+// type Language = "en" | "fr" | "es" | "ja" | "zh";
+
+// type TypeOfLanguage = typeof conf;
+// // type TypeOfLanguage = {
+// //     en: string;
+// //     fr: string;
+// //     es: string;
+// //     ja: string;
+// //     zh: string;
+// // }
+
+// type Language = keyof TypeOfLanguage;
+// // type Language = "en" | "fr" | "es" | "ja" | "zh"
+
+// type Language = keyof typeof conf;
+// // type Language = "en" | "fr" | "es" | "ja" | "zh"
+
