c4a49b4e2d1c937157ae80bdacff267566e96f50
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-10-05 10:13:23 +0900

    Create sample-4.ts
    https://typescriptbook.jp/tips/receive-an-object-return-an-object

---
+++ projects/typescriptbook-jp/tips/src/sample-4.ts
@@ -0,0 +1,56 @@
+/* ================================= */
+/* receive an object                 */
+/* return an object                  */
+/* ================================= */
+
+// function findUser(
+//   name?: string,
+//   age?: number,
+//   country?: string,
+//   isVip?: boolean
+// ): User {
+//   // ...
+// }
+
+// type UserInfo = {
+//   name?: string;
+//   age?: number;
+//   country?: string;
+//   isVip?: boolean;
+// };
+
+// function findUser(info: UserInfo): User {
+//   if (info.age >= 20) {
+//     // ...
+//   }
+//   // ...
+// }
+
+// function findUserByName({ name }: UserInfo): User {
+//   // ...
+// }
+
+// type UserInfo = {
+//   name?: string;
+//   age?: number;
+//   country?: string;
+//   nationality?: string;
+//   isVip?: boolean;
+// };
+
+// function findUserByName({ name, nationality }: UserInfo): User {
+//   // ...
+// }
+
+// type UserInfo = {
+//   name?: string;
+//   age?: number;
+//   country?: string;
+//   nationality?: string;
+//   isVip?: boolean;
+//   isRetired: boolean;
+// };
+
+// function findUser({ name, age, country, isRetired = false }: UserInfo): User {
+//   // ...
+// }
