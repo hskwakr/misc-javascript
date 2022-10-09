1f607d98cb207d6ed51003440a1eb6a6d4c15e14
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-10-09 17:16:54 +0900

    Add examples
    State: A Component's Memory https://beta.reactjs.org/learn/state-a-components-memory

--- projects/reactjs-org/state-a-components-memory/src/App.js
+++ projects/reactjs-org/state-a-components-memory/src/App.js
@@ -1,6 +1,15 @@
+import FeedbackForm1 from "./components/FeedbackForm1";
+import FeedbackForm2 from "./components/FeedbackForm2";
+import Form from "./components/Form";
+import Gallery from "./components/Gallery";
+
 function App() {
   return (
     <div className="App">
+      <Gallery />
+      <Form />
+      <FeedbackForm1 />
+      <FeedbackForm2 />
     </div>
   );
 }
