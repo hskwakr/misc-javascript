1d32dad5ee754699da124501ede8f322d74e1e63
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-10-11 13:19:14 +0900

    Add examples
    Updating Objects in State https://beta.reactjs.org/learn/updating-objects-in-state

--- projects/reactjs-org/updating-objects-in-state/src/App.js
+++ projects/reactjs-org/updating-objects-in-state/src/App.js
@@ -1,6 +1,17 @@
+import Canvas from "./components/Canvas";
+import Form1 from "./components/Form1";
+import Form2 from "./components/Form2";
+import MovingDot from "./components/MovingDot";
+import Scoreboard from "./components/Scoreboard";
+
 function App() {
   return (
     <div className="App">
+      <MovingDot />
+      <Form1 />
+      <Form2 />
+      <Scoreboard />
+      <Canvas />
     </div>
   );
 }
