47aa5424817d6afc01df5325dd3a9afd0422b330
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-10-14 11:10:51 +0900

    Add examples
    Sharing State Between Components https://beta.reactjs.org/learn/sharing-state-between-components

--- projects/reactjs-org/managing-state/sharing-state-between-components/src/App.js
+++ projects/reactjs-org/managing-state/sharing-state-between-components/src/App.js
@@ -1,6 +1,15 @@
+import Accordion1 from "./components/Accordion1";
+import Accordion2 from "./components/Accordion2";
+import FilterableList from "./components/FilterableList";
+import SyncedInputs from "./components/SyncedInputs";
+
 function App() {
   return (
     <div className="App">
+      <Accordion1 />
+      <Accordion2 />
+      <SyncedInputs />
+      <FilterableList />
     </div>
   );
 }
