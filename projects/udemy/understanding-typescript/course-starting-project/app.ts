40ca55a5352e3473671ffef2c44943da39a0053b
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-12-06 02:35:18 +0900

    Update app.ts
    24. Type Aliases / Custom Types

--- projects/udemy/understanding-typescript/course-starting-project/app.ts
+++ projects/udemy/understanding-typescript/course-starting-project/app.ts
@@ -1,25 +1,33 @@
-function combine(input1: number | string, input2: number | string) {
+type Combinable = number | string;
+type ConversionDescriptor = "as-number" | "as-string";
+
+function combine(
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: ConversionDescriptor
+) {
   let result;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
   return result;
+
+  // if (resultConversion === "as-number") {
+  //   return +result;
+  // } else {
+  //   return result.toString();
+  // }
 }
 
-function combine2<T extends number | string>(input1: T, input2: T): T {
-  let result: T;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = (input1 + input2) as T;
-  } else {
-    result = (input1.toString() + input2.toString()) as T;
-  }
-  return result;
-}
-
-const combinedAges = combine2(30, 26);
+const combinedAges = combine(30, 26, "as-number");
+console.log(combinedAges);
+const combinedStringAges = combine("30", "26", "as-number");
 console.log(combinedAges);
 
-const combineNames = combine2("Max", "Anna");
-console.log(combinedAges);
+const combineNames = combine("Max", "Anna", "as-string");
+console.log(combineNames);
