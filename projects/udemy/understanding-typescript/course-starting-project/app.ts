e27fe306cedd71f630955b11c9d5f93e824fd279
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-12-06 16:24:27 +0900

    Update app.ts
    28. Function Types & Callbacks

--- projects/udemy/understanding-typescript/course-starting-project/app.ts
+++ projects/udemy/understanding-typescript/course-starting-project/app.ts
@@ -6,9 +6,19 @@ function printResult(num: number): void {
   console.log("Result: " + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
 printResult(add2(5, 12));
 
 let combineValues: typeof add2;
 combineValues = add2;
 
 console.log(combineValues(8, 8));
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+  return result; // ignored
+});
