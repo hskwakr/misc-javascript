a363f0a0b54444397cd73ef0b8d94e24bcaaf4be
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-12-08 06:29:54 +0900

    Update app.ts
    50. Arrow Function

--- projects/udemy/understanding-typescript/next-gen/src/app.ts
+++ projects/udemy/understanding-typescript/next-gen/src/app.ts
@@ -4,19 +4,33 @@ const userName = "Max";
 let age = 30;
 age = 29;
 
-function add(a: number, b: number) {
-  let result;
-  result = a + b;
-  return result;
-}
+// function add(a: number, b: number) {
+//   let result;
+//   result = a + b;
+//   return result;
+// }
 // result = 1; // error
 
 // if (age > 20) {
-//   var isOld = true;  
+//   var isOld = true;
 // }
 // isOld = false; // can access
 
 // if (age > 20) {
-//   let isOld = true;  
+//   let isOld = true;
 // }
 // isOld = false; // error
+
+// const add = (a: number, b: number) => {
+//   return a + b;
+// };
+const add = (a: number, b: number) => a + b;
+console.log(add(2, 5));
+
+const printOutput: (a: string | number) => void = (output) =>
+  console.log(output);
+
+const button = document.querySelector("button");
+if (button) {
+  button.addEventListener("click", event => console.log(event));
+}
