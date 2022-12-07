0994aa72c2901cc03461cd2e24d5f8084957c4c3
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-12-08 06:22:38 +0900

    Update app.ts
    49. "let" and "const"

--- projects/udemy/understanding-typescript/next-gen/src/app.ts
+++ projects/udemy/understanding-typescript/next-gen/src/app.ts
@@ -1,1 +1,22 @@
-// Code goes here!
+const userName = "Max";
+// userName ="Maximilian" // error
+
+let age = 30;
+age = 29;
+
+function add(a: number, b: number) {
+  let result;
+  result = a + b;
+  return result;
+}
+// result = 1; // error
+
+// if (age > 20) {
+//   var isOld = true;  
+// }
+// isOld = false; // can access
+
+// if (age > 20) {
+//   let isOld = true;  
+// }
+// isOld = false; // error
