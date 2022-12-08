e4fe0beed6bbae23dbb25c7dab4860b09949af40
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-12-09 04:44:19 +0900

    Update app.ts
    67. Getters & Setters

--- projects/udemy/understanding-typescript/cl-interfaces/src/app.ts
+++ projects/udemy/understanding-typescript/cl-interfaces/src/app.ts
@@ -29,8 +29,25 @@ class ITDepartment extends Department {
 }
 
 class AccountingDepartment extends Department {
+  private lastReport: string;
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error("No report found.");
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error("Please pass in a valid value!");
+    }
+    this.addReport(value);
+  }
+
   constructor(id: string, private reports: string[]) {
     super(id, "Accounting");
+    this.lastReport = reports[0];
   }
 
   addEmployees(name: string): void {
@@ -42,6 +59,7 @@ class AccountingDepartment extends Department {
 
   addReport(text: string) {
     this.reports.push(text);
+    this.lastReport = text;
   }
 
   printReport() {
@@ -64,12 +82,17 @@ console.log(it);
 
 const accounting = new AccountingDepartment("d2", []);
 
+// console.log(accounting.mostRecentReport); // throw error
+
 accounting.addEmployees("Max");
 accounting.addEmployees("Manu");
 
+// accounting.mostRecentReport = ""; // throw error
+accounting.mostRecentReport = "Year End Report";
 accounting.addReport("Something went wrong...");
-accounting.printReport();
+console.log(accounting.mostRecentReport);
 
+accounting.printReport();
 accounting.printEmployeeInformation();
 
 // const accountingCopy = { describe: accounting.describe };
