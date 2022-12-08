86d0e517c97b47f625b4261b8aeef60b0459cbc0
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-12-09 04:51:40 +0900

    Update app.ts
    68. Static Methods & Properties

--- projects/udemy/understanding-typescript/cl-interfaces/src/app.ts
+++ projects/udemy/understanding-typescript/cl-interfaces/src/app.ts
@@ -1,10 +1,16 @@
 class Department {
+  static fiscalYear = 2020;
   // private id: string;
   // private name: string;
   protected employees: string[] = [];
 
   constructor(private readonly id: string, public name: string) {
     // this.name = name;
+    console.log(Department.fiscalYear);
+  }
+
+  static createEmployee(name: string) {
+    return { name: name };
   }
 
   describe(this: Department) {
@@ -79,6 +85,9 @@ it.name = "NEW NAME";
 it.printEmployeeInformation();
 
 console.log(it);
+
+const employee1 = Department.createEmployee("Max");
+console.log(employee1, Department.fiscalYear);
 
 const accounting = new AccountingDepartment("d2", []);
 
