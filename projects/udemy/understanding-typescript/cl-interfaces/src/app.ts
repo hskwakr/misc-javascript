2f19c54eecb91be875a55237edb162bd2aec62f4
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-12-08 16:49:45 +0900

    Update app.ts
    61. Constructor Functions & The "this" keyword

--- projects/udemy/understanding-typescript/cl-interfaces/src/app.ts
+++ projects/udemy/understanding-typescript/cl-interfaces/src/app.ts
@@ -4,7 +4,17 @@ class Department {
   constructor(name: string) {
     this.name = name;
   }
+
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
 }
 
 const accounting = new Department("Accounting");
-console.log(accounting);
+accounting.describe();
+
+// const accountingCopy = { describe: accounting.describe };
+// accountingCopy.describe(); // error
+
+const accountingCopy = { name: "DUMMY", describe: accounting.describe };
+accountingCopy.describe();
