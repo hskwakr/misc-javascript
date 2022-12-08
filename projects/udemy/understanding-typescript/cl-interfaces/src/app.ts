14645731a43753ff31d161b11115a9bbf55b4501
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-12-08 16:59:57 +0900

    Update app.ts
    62. "private" and "public" Access Modifiers

--- projects/udemy/understanding-typescript/cl-interfaces/src/app.ts
+++ projects/udemy/understanding-typescript/cl-interfaces/src/app.ts
@@ -1,5 +1,6 @@
 class Department {
   name: string;
+  private employees: string[] = [];
 
   constructor(name: string) {
     this.name = name;
@@ -8,13 +9,30 @@ class Department {
   describe(this: Department) {
     console.log("Department: " + this.name);
   }
+
+  addEmployees(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department("Accounting");
+
+accounting.addEmployees("Max");
+accounting.addEmployees("Manu");
+
+// accounting.employees[2] = "Anna"; // error
+
 accounting.describe();
+accounting.name = "NEW NAME";
+accounting.printEmployeeInformation();
 
 // const accountingCopy = { describe: accounting.describe };
 // accountingCopy.describe(); // error
 
-const accountingCopy = { name: "DUMMY", describe: accounting.describe };
-accountingCopy.describe();
+// const accountingCopy = { name: "DUMMY", describe: accounting.describe };
+// accountingCopy.describe();
