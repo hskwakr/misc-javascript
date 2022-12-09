7f30cf4ddae3e33f0e03423bf09c46e92d520182
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-12-09 11:53:43 +0900

    Update app.ts
    77. Interfaces as Function Types

--- projects/udemy/understanding-typescript/cl-interfaces/src/app.ts
+++ projects/udemy/understanding-typescript/cl-interfaces/src/app.ts
@@ -20,6 +20,16 @@
 
 interface AnotherInterface {}
 
+// type AddFn = (a: number, b: number) => number;
+interface AddFn {
+  (a: number, b: number): number;
+}
+
+let add: AddFn;
+add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
 interface Named {
   readonly name: string;
 }
