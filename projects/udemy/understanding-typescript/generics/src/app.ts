9f6c1064436cffc539488d3381f3250863e636db
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-12-10 07:16:27 +0900

    Update app.ts
    95. Creating a Generic Function

--- projects/udemy/understanding-typescript/generics/src/app.ts
+++ projects/udemy/understanding-typescript/generics/src/app.ts
@@ -1,3 +1,5 @@
+// Built-in Generics & What are Generics?
+
 // const names: Array<string> = []; // string
 // // names[0].split(" ");
 
@@ -11,3 +13,22 @@
 // promise.then((data) => {
 //   // data.split(" "); // error
 // });
+
+// Creating a Generic Function
+
+// function merge(objA: object, objB: object) {
+//   return Object.assign(objA, objB);
+// }
+// // console.log(merge({ name: "Max" }, { age: 30 }));
+
+// const mergedObj = merge({ name: "Max" }, { age: 30 });
+
+// // can't access
+// // mergedObj.name;
+// // mergedObj.age;
+
+function merge<T extends {}, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: "Max" }, { age: 30 });
