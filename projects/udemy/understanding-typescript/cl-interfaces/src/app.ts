45571f5013e0d319171e2d6eb6e3c726fca9e48b
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-12-09 09:19:44 +0900

    Update app.ts
    73. Using Interfaces with Classes

--- projects/udemy/understanding-typescript/cl-interfaces/src/app.ts
+++ projects/udemy/understanding-typescript/cl-interfaces/src/app.ts
@@ -1,19 +1,38 @@
-interface Person {
-  // name: string = "Max"; // error
-  name: string; // error
-  age: number;
+// interface Person {
+//   // name: string = "Max"; // error
+//   name: string;
+//   age: number;
 
+//   greet(phrase: string): void;
+// }
+
+// let user1: Person;
+// user1 = {
+//   name: "Max",
+//   age: 30,
+
+//   greet(phrase: string): void {
+//     console.log(phrase + " " + this.name);
+//   },
+// };
+
+// user1.greet("Hi there - I am");
+
+interface Greetable {
+  name: string;
   greet(phrase: string): void;
 }
 
-let user1: Person;
-user1 = {
-  name: "Max",
-  age: 30,
+interface AnotherInterface {}
+
+class Person implements Greetable, AnotherInterface {
+  constructor(public name: string) {}
 
   greet(phrase: string): void {
     console.log(phrase + " " + this.name);
-  },
-};
+  }
+}
 
-user1.greet("Hi there - I am");
+let user1: Greetable;
+user1 = new Person("Max");
+user1.greet("Hi,");
