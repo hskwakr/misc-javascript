53543de1782e4af4b6302ce0141df24206c01fbf
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-12-09 13:28:50 +0900

    Update app.ts
    84. More on Type Guards

--- projects/udemy/understanding-typescript/adv-types/src/app.ts
+++ projects/udemy/understanding-typescript/adv-types/src/app.ts
@@ -32,3 +32,66 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+// type guard 1
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+// type guard 2
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log("Name: " + emp.name);
+
+  if ("privileges" in emp) {
+    console.log("Privilegese: " + emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("Start Date: " + emp.startDate);
+  }
+}
+
+class Car {
+  drive() {
+    console.log("Driving...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving...");
+  }
+
+  loadCargo(amount: number) {
+    console.log("Loading cargo..." + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+// // type guard with class 1
+// function useVehicle(vehicle: Vehicle) {
+//   vehicle.drive();
+//   if ("loadCargo" in vehicle) {
+//     vehicle.loadCargo(1000);
+//   }
+// }
+
+// type guard with class 2
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
