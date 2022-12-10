c27ad457ec3f00cc331edd1a9a14487e91f59621
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-12-11 07:59:55 +0900

    Update app.ts
    109. Diving into Property Decorators

--- projects/udemy/understanding-typescript/decorators/src/app.ts
+++ projects/udemy/understanding-typescript/decorators/src/app.ts
@@ -38,3 +38,33 @@ class Person {
 
 const p = new Person();
 console.log(p);
+
+// Diving into Property Decorators
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property Decorator!");
+  console.log(target, propertyName);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number = 0;
+
+  set price(val: number) {
+    if (val >= 0) {
+      this._price = val;
+    } else {
+      throw new Error("Invalid price - should be positive!");
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this.price = p;
+  }
+
+  getPriceWithTax(tax: number) {
+    return this._price * (1 + tax);
+  }
+}
