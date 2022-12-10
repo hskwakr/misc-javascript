d81ff9868e4756b19915c4cb5e625b8085c977ae
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-12-10 12:12:41 +0900

    Update app.ts
    98. Another Generic Function

--- projects/udemy/understanding-typescript/generics/src/app.ts
+++ projects/udemy/understanding-typescript/generics/src/app.ts
@@ -41,3 +41,25 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 }
 
 // const mergedObj = merge({ name: "Max" }, 30); // error
+
+// Another Generic Function
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value";
+  if (element.length === 1) {
+    descriptionText = "Got 1 element";
+  } else if (element.length > 1) {
+    descriptionText = "Got " + element.length + " elements";
+  }
+
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe("Hi there!"));
+console.log(countAndDescribe(["Sports", "Cooking"]));
+console.log(countAndDescribe([]));
+// console.log(countAndDescribe(10)); // error
