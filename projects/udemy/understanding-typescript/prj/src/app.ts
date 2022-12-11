23919e08b47e4e0fcc1b4974b5511d63c32245f4
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-12-12 06:04:29 +0900

    Update app.ts
    122. DOM Element Selection & OOP Rendering

--- projects/udemy/understanding-typescript/prj/src/app.ts
+++ projects/udemy/understanding-typescript/prj/src/app.ts
@@ -1,1 +1,34 @@
-// Code goes here!
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    const templateEl = document.getElementById('project-input');
+    if (templateEl) {
+      this.templateElement = templateEl as HTMLTemplateElement;
+    } else {
+      throw new Error('Missing Template Element');
+    }
+
+    const hostEl = document.getElementById('app');
+    if (hostEl) {
+      this.hostElement = hostEl as HTMLDivElement;
+    } else {
+      throw new Error('Missing App Element');
+    }
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+}
+
+const prjInput = new ProjectInput();
