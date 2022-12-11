7f36fad936c7d0b5c2ae33f79b31070462973cd6
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-12-12 08:43:23 +0900

    Update app.ts
    123. Interacting with DOM Elements

--- projects/udemy/understanding-typescript/prj/src/app.ts
+++ projects/udemy/understanding-typescript/prj/src/app.ts
@@ -1,9 +1,18 @@
 class ProjectInput {
+  // Template element which have form componet
   templateElement: HTMLTemplateElement;
+  // Element to display
   hostElement: HTMLDivElement;
+  // Element to attacth to host element
   element: HTMLFormElement;
 
+  // Form components
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
   constructor() {
+    // Get template element
     const templateEl = document.getElementById('project-input');
     if (templateEl) {
       this.templateElement = templateEl as HTMLTemplateElement;
@@ -11,6 +20,7 @@ class ProjectInput {
       throw new Error('Missing Template Element');
     }
 
+    // Get host element
     const hostEl = document.getElementById('app');
     if (hostEl) {
       this.hostElement = hostEl as HTMLDivElement;
@@ -18,12 +28,39 @@ class ProjectInput {
       throw new Error('Missing App Element');
     }
 
+    // Get contents of templete element
     const importedNode = document.importNode(
       this.templateElement.content,
       true
     );
+
+    // Get the element to attach
+    // The first element in contents should be form element
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+
+    // Get form components
+    this.titleInputElement = this.element.querySelector(
+      '#title'
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      '#description'
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      '#people'
+    ) as HTMLInputElement;
+
+    this.configure();
     this.attach();
+  }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler.bind(this));
   }
 
   private attach() {
