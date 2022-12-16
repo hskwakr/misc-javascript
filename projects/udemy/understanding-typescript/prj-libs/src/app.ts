ecee9c9152cca43d8835bde619a152f60d8a5c6e
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-12-17 07:40:37 +0900

    Update app.ts
    170. Getting User Input

---
+++ projects/udemy/understanding-typescript/prj-libs/src/app.ts
@@ -0,0 +1,21 @@
+// Get form element
+const form = document.querySelector('form');
+if (!form) {
+  throw new Error('Missing form element');
+}
+
+// Get input element
+const addressInput = document.getElementById('address') as HTMLInputElement;
+if (!addressInput) {
+  throw new Error('Missing address input element');
+}
+
+// Event handler to search address
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+
+  const enteredAddress = addressInput.value;
+}
+
+// Register events
+form.addEventListener('submit', searchAddressHandler);
