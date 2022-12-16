cd98f48042e70c1c01ed1325fdcb92c0609af8f1
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-12-16 16:32:53 +0900

    Update example of UT
    164. No Types Needed: class-transformer

--- projects/udemy/understanding-typescript/libs/src/app.ts
+++ projects/udemy/understanding-typescript/libs/src/app.ts
@@ -1,6 +1,26 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 
-declare var GLOBAL: string;
+// declare var GLOBAL: string;
 
-console.log(_.shuffle([1, 2, 3]));
-console.log(GLOBAL);
+// console.log(_.shuffle([1, 2, 3]));
+// console.log(GLOBAL);
+
+import 'reflect-metadata';
+import { plainToInstance } from 'class-transformer';
+import { Product } from './product.model';
+
+// const p1 = new Product('A Book', 12.99);
+
+const products = [
+  { title: 'A Book', price: 12.99 },
+  { title: 'A Book', price: 10.99 },
+];
+
+// const loadedProducts = products.map((prod) => {
+//   return new Product(prod.title, prod.price);
+// });
+const loadedProducts = plainToInstance(Product, products);
+
+for (const prod of loadedProducts) {
+  console.log(prod.getInformation());
+}
