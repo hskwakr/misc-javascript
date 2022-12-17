9a31a4a5d9035999c915500c6bc8af60c2b856bb
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-12-17 09:19:38 +0900

    Update example of UT
    172. Using Axios to Fetch Coordinates for an Entered Address

--- projects/udemy/understanding-typescript/prj-libs/src/app.ts
+++ projects/udemy/understanding-typescript/prj-libs/src/app.ts
@@ -1,5 +1,16 @@
+import axios from 'axios';
+
 // Get the api key
 const API_KEY = process.env.GOOGLE_API_KEY;
+
+// Set api url
+const API_URL_BASE = 'https://maps.googleapis.com/maps/api/geocode/';
+
+// Define api response type
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: 'OK' | 'ZERO_RESULTS';
+};
 
 // Get form element
 const form = document.querySelector('form');
@@ -14,10 +25,29 @@ if (!addressInput) {
 }
 
 // Event handler to search address
-function searchAddressHandler(event: Event) {
+async function searchAddressHandler(event: Event) {
   event.preventDefault();
 
-  // const enteredAddress = addressInput.value;
+  // Get input data
+  const enteredAddress = addressInput.value;
+
+  // Set the url to request
+  const url =
+    API_URL_BASE + `json?address=${encodeURI(enteredAddress)}&key=${API_KEY}`;
+
+  try {
+    // Send request to search
+    const res = await axios.get<GoogleGeocodingResponse>(url);
+    // console.log(res);
+    if (res.data.status !== 'OK') {
+      throw new Error('Could not fetch location!');
+    }
+    
+    // Get a location data
+    // const coordinates = res.data.results[0].geometry.location;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 // Register events
