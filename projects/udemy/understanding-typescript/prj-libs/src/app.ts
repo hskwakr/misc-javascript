5220080f2edf609cbaaebafc628b8bf81698e0f3
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-12-17 10:09:45 +0900

    Update example of UT
    173. Rendering a Map with Google Maps (incl. Types!)

--- projects/udemy/understanding-typescript/prj-libs/src/app.ts
+++ projects/udemy/understanding-typescript/prj-libs/src/app.ts
@@ -1,7 +1,18 @@
 import axios from 'axios';
+import { Loader } from '@googlemaps/js-api-loader';
 
 // Get the api key
 const API_KEY = process.env.GOOGLE_API_KEY;
+if (!API_KEY) {
+  throw new Error('Missing Google API key');
+}
+
+// Get the map loader
+const loader = new Loader({
+  apiKey: API_KEY,
+  version: 'weekly',
+  libraries: ['places'],
+});
 
 // Set api url
 const API_URL_BASE = 'https://maps.googleapis.com/maps/api/geocode/';
@@ -36,15 +47,32 @@ async function searchAddressHandler(event: Event) {
     API_URL_BASE + `json?address=${encodeURI(enteredAddress)}&key=${API_KEY}`;
 
   try {
+    // Get map element
+    const mapEl = document.getElementById('map');
+    if (!mapEl) {
+      throw new Error('Missing map element');
+    }
+
     // Send request to search
     const res = await axios.get<GoogleGeocodingResponse>(url);
     // console.log(res);
     if (res.data.status !== 'OK') {
       throw new Error('Could not fetch location!');
     }
-    
+
     // Get a location data
-    // const coordinates = res.data.results[0].geometry.location;
+    const coordinates = res.data.results[0].geometry.location;
+
+    // Load the map
+    const googleMap = await loader.load();
+
+    // Render map
+    const map = new googleMap.maps.Map(mapEl, {
+      center: coordinates,
+      zoom: 10,
+    });
+
+    new google.maps.Marker({ position: coordinates, map: map });
   } catch (error) {
     console.log(error);
   }
