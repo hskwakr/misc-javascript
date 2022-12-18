d67cc7838e40a17bbb2abadf6bed6dc35002860c
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-12-19 04:45:34 +0900

    Update example of UT
    185. Adding Styling

--- projects/udemy/understanding-typescript/prj-react/src/components/NewTodo.tsx
+++ projects/udemy/understanding-typescript/prj-react/src/components/NewTodo.tsx
@@ -1,5 +1,7 @@
 import React, { useRef } from 'react';
 
+import './NewTodo.css';
+
 interface NewTodoProps {
   onAddTodo: (text: string) => void;
 }
@@ -16,7 +18,7 @@ export default function NewTodo({ onAddTodo }: NewTodoProps): JSX.Element {
 
   return (
     <form onSubmit={todoSubmitHandler}>
-      <div>
+      <div className='form-control'>
         <label htmlFor='todo-text'>Todo Text</label>
         <input type='text' id='todo-text' ref={textInputRef} />
       </div>
